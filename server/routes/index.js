3d3b33fea6daba9eb0ad3cdd1984ef64dde4863e
Harley Santos Garzón <santo.gh@gmail.com>
2020-03-10 17:58:23 -0500

    fix(*): ajuste ruta static files
    Se configura express para vincular archivos estaticos en el demo

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const app = express();
 /**
  * MIDDELWARES
  */
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 app.set("view engine", "pug");
 // ------------------------- COMMAND
 const writeDB = require("./../../commands");
