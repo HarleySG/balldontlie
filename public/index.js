5181a802e23f66069d5148537f14d07d59be0385
Harley Santos Garzón <santo.gh@gmail.com>
2020-03-10 19:21:19 -0500

    test(*): prueba de formData en cliente
    Se realiza una prueba para iniciar el filtro en cliente

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".navbar-burger"),
     0
   );
+  const $filter = document.getElementById("form_filter");
   if ($navbarBurgers.length > 0) {
     $navbarBurgers.forEach(el => {
       el.addEventListener("click", () => {
@@ -12,5 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
         $target.classList.toggle("is-active");
       });
     });
+  }
+  if ($filter) {
+    $filter.addEventListener("submit", findPlayer);
+    $filter.addEventListener("formdata", event => {
+      event.preventDefault();
+      const data = event.formData;
+      // get the data
+      const entries = [...data.entries()];
+      console.log(entries);
+
+      const values = [...data.values()];
+      console.log(values);
+      fetch("https://balldontlie.io/api/v1/players?search=" + values).then(
+        res => {
+          console.log("res", res);
+        }
+      );
+    });
+  }
+  function findPlayer(event) {
+    event.preventDefault();
+    new FormData($filter);
   }
 });
