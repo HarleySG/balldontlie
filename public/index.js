1a8db9f0e8dfd2123e11f24630aa89c580948329
Harley Santos Garzón <santo.gh@gmail.com>
2020-03-11 12:00:34 -0500

    refactor(cliente js): se ajusta validaciones de cliente

--- public/index.js
+++ public/index.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".navbar-burger"),
     0
   );
-  const $filter = document.getElementById("form_filter");
   if ($navbarBurgers.length > 0) {
     $navbarBurgers.forEach(el => {
       el.addEventListener("click", () => {
@@ -14,26 +13,110 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   }
-  if ($filter) {
-    $filter.addEventListener("submit", findPlayer);
-    $filter.addEventListener("formdata", event => {
-      event.preventDefault();
+  const $filter = document.getElementById("form_filter");
+  const $create = document.getElementById("form_create");
+  const $teamList = document.getElementById("form_teamList");
+  const $teamInputs = document.getElementById("form-teamvalues");
+  function createPlayer() {
+    const input = document.forms.namedItem("form_create");
+    input.addEventListener("submit", findPlayer);
+    input.addEventListener("formdata", formdataFunc);
+    function formdataFunc() {
       const data = event.formData;
-      // get the data
-      const entries = [...data.entries()];
-      console.log(entries);
-
-      const values = [...data.values()];
-      console.log(values);
-      fetch("https://balldontlie.io/api/v1/players?search=" + values).then(
-        res => {
+      let playerModel = {};
+      for (var pair of data.entries()) {
+        playerModel[pair[0]] = pair[1];
+      }
+      console.log("createPlayer -> playerModel", playerModel);
+      fetch("http://localhost:4200/addPlayer", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: playerModel
+      })
+        .then(res => {
+          console.log("status", res.status);
+          return res.json();
+        })
+        .then(res => {
           console.log("res", res);
-        }
-      );
+        })
+        .catch(err => {
+          console.error("res", err);
+        });
+    }
+    function findPlayer(event) {
+      event.preventDefault();
+      new FormData(input);
+    }
+  }
+
+  // Capturar la entrada del input
+  function domFilter(inputElement, selector, selectorContainer) {
+    if (inputElement) {
+      inputElement.addEventListener("keyup", e => {
+        if (e.key === "Escape") e.target.value = "";
+        compare(e.target.value.toUpperCase(), selector, selectorContainer);
+      });
+    }
+  }
+  // Mostrar los elementos que coincidan con esa entrada (ocultar los que no)
+  function compare(filterText, selectorElement, selectorContainer) {
+    let searchElements = document.querySelectorAll(selectorElement);
+    let searchContainers = document.querySelectorAll(selectorContainer);
+    searchElements.forEach(el => {
+      el.textContent.toUpperCase().includes(filterText)
+        ? (el.dataset.visible = "true")
+        : (el.dataset.visible = "false");
+    });
+    searchContainers.forEach(el => {
+      el.textContent.toUpperCase().includes(filterText)
+        ? (el.dataset.visible = "true")
+        : (el.dataset.visible = "false");
     });
   }
-  function findPlayer(event) {
-    event.preventDefault();
-    new FormData($filter);
-  }
+
+  domFilter($filter, ".js-table-row", ".js-table");
+  createPlayer($create);
+  (function() {
+    if ($teamList) {
+      const team_db = localStorage.getItem("team_db");
+      async function setTeamList(teamList) {
+        await teamList.map(team => {
+          const option = document.createElement("option");
+          option.value = team.id;
+          option.textContent = team.name;
+          $teamList.appendChild(option);
+        });
+      }
+      if (!team_db) {
+        fetch("https://www.balldontlie.io/api/v1/teams")
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            localStorage.setItem("team_db", JSON.stringify(data.data));
+            setTeamList(data.data);
+          });
+      } else {
+        setTeamList(JSON.parse(team_db));
+      }
+      $teamList.addEventListener("change", e => {
+        const teamSelected = JSON.parse(team_db).find(team => {
+          if (team.id == e.target.value) {
+            return team;
+          }
+        });
+        Object.keys(teamSelected).map(val => {
+          const input = document.querySelector(
+            `#${$teamInputs.id} input[name="${val}"]`
+          );
+          if ($teamInputs) {
+            input && (input.value = teamSelected[val]);
+          }
+        });
+      });
+    }
+  })();
 });
