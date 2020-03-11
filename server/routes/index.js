753fa6955208530f9b2a88d749d34545ba3a7006
Harley Santos Garzón <santo.gh@gmail.com>
2020-03-11 12:01:43 -0500

    feat(routes): se configura ruta addPlayer
    Pendiente registro de usuario en local db

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 // ------------------------- API
 const API = require("./../../API");
 /**
@@ -7,6 +8,7 @@ const API = require("./../../API");
  */
 app.use(express.static("public"));
 app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: false })).use(bodyParser.json());
 // ------------------------- PORT
 const PORT = process.env.PORT;
 // ------------------------- ROUTES
@@ -36,7 +38,7 @@ app.get("/", (req, res) => {
   });
 });
 app.get("/new", (req, res) => {
-  res.render("index", {
+  res.render("new", {
     title: "New - player",
     message: "Create new player!",
     menu: [
@@ -46,6 +48,35 @@ app.get("/new", (req, res) => {
     ]
   });
 });
+
+app.post("/addPlayer", (req, res, next) => {
+  const body = req.body;
+  console.log("body", body);
+  const modelPlayer = {
+    id: 14,
+    first_name: body.first_name,
+    height_feet: body.height_feet == "" ? null : body.height_feet,
+    height_inches: body.height_inches == "" ? null : body.height_inches,
+    last_name: body.last_name,
+    position: body.position,
+    team: {
+      id: body.team_id,
+      abbreviation: body.abbreviation,
+      city: body.city,
+      conference: body.conference,
+      division: body.division,
+      full_name: body.full_name,
+      name: body.name
+    },
+    weight_pounds: null
+  };
+  res.json({
+    success: true,
+    message: "User saved",
+    data: modelPlayer
+  });
+});
+
 app.listen(PORT, () => {
   console.log(` Puerto ${PORT} escuchando... `);
 });
