306157229cf311e03227fdf3efc72626da5769e7
Harley Santos Garzón <santo.gh@gmail.com>
2020-03-10 19:17:42 -0500

    refactor(get.js): cambio de servicio query

--- API/get.js
+++ API/get.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
-
-module.exports = ENDPOINT => {
-  return axios.get(process.env.API + ENDPOINT).then(response => {
-    return { status: response.status, data: response.data.data };
-  });
+module.exports = {
+  API: ENDPOINT => {
+    return axios.get(process.env.API + ENDPOINT).then(response => {
+      return { status: response.status, data: response.data.data };
+    });
+  },
+  local: () => require("../db/players.json")
 };
